ca021bd57fa9a208d7e10665de700cf711ec10a3
Ritvik Saraf <13ritvik@gmail.com>
2019-03-05 02:12:14 +0530

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "whatsapp-cli",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Ritvik Saraf",
     author_email = "13ritvik@gmail.com",
     description = "CLI for whatsapp",
